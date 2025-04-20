4c022614746bc9c110ebf029a8b7f7bbd8caf7ca
loki <Abdohamdy4587gmail.com>
2025-04-20 14:59:47 +0200

    did my part

---
+++ lib/screens/sett.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class sett extends StatelessWidget {
+  const sett({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('setting')),
+      body: Center(
+        
+          child: Text('Back to Home'),
+        ),
+      
+    );
+  }
+}
