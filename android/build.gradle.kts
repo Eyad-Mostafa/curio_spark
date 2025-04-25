663c6aaa536440df14f006962fdb20a806c5e5ed
Eyad Mostafa <your.email@example.com>
2025-04-25 23:40:14 +0300

    refactor: update Gradle configuration and dependencies for improved compatibility

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,46 +1,21 @@
-plugins {
-    id("com.android.application") version "8.3.0"  // Specify the version of the Android plugin
-    id("org.jetbrains.kotlin.android") version "2.0.20"
-}
-
-android {
-    namespace = "com.example.curio_spark"
-    compileSdk = 34
-    javaCompileOptions {
-            annotationProcessorOptions {
-                includeCompileClasspath true
-            }
-        }
-    ndkVersion = flutter.ndkVersion
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
-    defaultConfig {
-        applicationId = "com.example.curio_spark"
-        minSdk = 21
-        targetSdk = 34
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
-        }
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
 }
 
-flutter {
-    source = "../.."  // This points to your Flutter source directory
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
 }
 
-dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
